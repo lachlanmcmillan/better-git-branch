c40b37353a72374feeb6188e12821006c51e9452
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2020-05-16 10:16:15 +1000

    Call git from rust

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::process::Command;
 
 fn main() {
     let output = Command::new("git")
-        .args(&["for-each-ref", "--count=20"])
+        .args(&["for-each-ref", "--count=20", "--format='%(refname:short)'"])
         .output().expect("failed to execute process");
-    println!("output = {:?}", output.stdout);
+    // assuming the stdout Vec is utf8
+    let output_as_string = String::from_utf8(output.stdout).unwrap();
+    // let lines = output_as_string.split('\n');
+    println!("{}", output_as_string);
 }
 
 //git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format='%(refname:short)'
