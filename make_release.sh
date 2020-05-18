896ffbd340da44919c071d789c302e676b38fc6d
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2020-05-19 08:10:01 +1000

    Release v1.0.1
    Testing the release process

---
+++ make_release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION="v1.0.1" # todo read this from cargo.toml
+BINARY="./better-git-branch"
+OUTPUT_FILE="better-git-branch-${VERSION}.tar.gz"
+HASH_FILE="better-git-branch-${VERSION}.tar.gz.sha256.txt"
+
+# make the binary
+cargo build --release
+
+cd ./target/release
+
+# make the tarball
+tar -czf ${OUTPUT_FILE} ${BINARY}
+
+# hash
+shasum -a 256 ${OUTPUT_FILE} > ${HASH_FILE}
