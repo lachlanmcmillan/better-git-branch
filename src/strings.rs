b0d3debc26da2ec4259f2e3c7fabb39285ea61a5
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2020-06-17 12:29:02 +1000

    update strings

--- src/strings.rs
+++ src/strings.rs
@@ -1,4 +1,3 @@
-pub const DELETE_BRANCH_PROHIBITED: &str = "cannot delete current branch";
+pub const DELETE_BRANCH_PROHIBITED: &str = "Error: Cannot delete current branch";
 pub const HIGHLIGHT_SYMBOL: &str = "* ";
 pub const GIT_FAIL: &str = "failed to call git executable";
-pub const PANIC_CHECKOUT: &str = "error: checkout called without a selected branch";
